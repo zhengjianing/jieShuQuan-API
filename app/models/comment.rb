756d999d0f440e1633ede49837b679956dd61410
Zheng Jia Ning <jianingzheng@gmail.com>
2014-10-06 17:52:06 +0800

    return full time for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,7 @@ class Comment < ActiveRecord::Base
   def comment_info
     {
         user_name: self.user_name,
-        comment_date: self.created_at.time.strftime('%Y-%m-%d'),
+        comment_date: self.created_at.time.strftime('%Y-%m-%d %H:%M:%S'),
         content: self.content
     }
   end
