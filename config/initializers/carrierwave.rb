f9b2424fa401000b8032692e99ed80149f87f97b
Zheng Jia Ning <jianingzheng@gmail.com>
2014-10-11 10:45:24 +0800

    use another qiniu account

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 ::CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = "YX8NmEu7UJmewEUCLLixW4ZfKxAfg_20sZhhVS7b"
-  config.qiniu_secret_key    = 'o2tBq4RUtP9fuOVt-5K3fnp_WSQ3QfD8ddzwsZC2 '
-  config.qiniu_bucket        = "jieshuquan"
-  config.qiniu_bucket_domain = "jieshuquan.qiniudn.com"
+  config.qiniu_access_key    = "oty9RerOy-rOdvkiI9YPnLNeqJP4tJ0I3SjW9UqJ"
+  config.qiniu_secret_key    = 'zaKg0_T_DmVHRheE3DWx1ZAZohHEEVz4Q5gxaNr5'
+  config.qiniu_bucket        = "jieshuquan-ios"
+  config.qiniu_bucket_domain = "jieshuquan-ios.qiniudn.com"
   config.qiniu_bucket_private= true
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
